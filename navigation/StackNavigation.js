27c2eb9a024e0d45cc312d9e6234b094b5b12b20
mohitsharma9001 <mohitsharma90015656@gmail.com>
2025-02-07 21:45:08 +0530

    changes done

--- navigation/StackNavigation.js
+++ navigation/StackNavigation.js
@@ -1,27 +1,32 @@
-
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react'
-import BottomTabNavigation from './BottomTabNavigation';
-import BlogDetails from '../screens/BlogDetails';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import BottomTabNavigation from "./BottomTabNavigation";
+import BlogDetails from "../screens/BlogDetails";
+import Bookmarked from "../screens/Bookmarked";
 const StackNavigation = () => {
-    const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen
-            name="Bottom Navigation"
-            component={BottomTabNavigation}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="blogDetails"
-            component={BlogDetails}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="Bottom Navigation"
+          component={BottomTabNavigation}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="blogDetails"
+          component={BlogDetails}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="bookmarked"
+          component={Bookmarked}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default StackNavigation
+export default StackNavigation;
